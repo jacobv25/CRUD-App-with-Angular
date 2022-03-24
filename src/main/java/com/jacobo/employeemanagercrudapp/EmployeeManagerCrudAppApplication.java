f91aba1eced48b78e995b9e29646bd8ad636a09e
Jacob Valenzuela <valen306@cougars.csusm.edu>
2022-03-24 13:37:15 -0700

    Initial commit

---
+++ src/main/java/com/jacobo/employeemanagercrudapp/EmployeeManagerCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.jacobo.employeemanagercrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagerCrudAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagerCrudAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jacobo/employeemanagercrudapp/EmployeeManagerCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacobo.employeemanagercrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagerCrudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
